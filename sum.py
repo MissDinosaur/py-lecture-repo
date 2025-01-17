9d4e5a15621ac27356c78a3e1cae3d833a968fe2
unknown <1157363323@qq.com>
2025-01-17 12:10:05 +0100

    delete the file

--- sum.py
+++
@@ -1,5 +0,0 @@
-if __name__ = "__main__":
-    a = int(input("Please enter the first number: "))
-    b = int(input("Please enter the second number: "))
-
-    print(f"the first number plus the second number equals: f{a+b}")
