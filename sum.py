2946d14b7dd88af44d9606c9ec42e568aa8b8058
unknown <1157363323@qq.com>
2025-01-17 11:36:34 +0100

    add tow number

---
+++ sum.py
@@ -0,0 +1,5 @@
+if __name__ = "__main__":
+    a = int(input("Please enter the first number: "))
+    b = int(input("Please enter the second number: "))
+
+    print(f"the first number plus the second number equals: f{a+b}")
